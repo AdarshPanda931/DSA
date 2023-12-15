dca2a6674a1b9f3c81f2eac6eea94ba2d11734f0
“AdarshPanda931” <adarshpanda931@gmail.com>
2023-12-15 20:58:01 +0530

    adding two more array question

---
+++ 2.check_if_arr_is_sorted.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+bool funct(int arr[], int n){
+    for (int i=0; i<n; i++){
+        if (arr[i]>arr[i+1])
+        return false;
+    }
+    return true;
+}
+
+int main() {
+    int n;
+    cout <<"enter size: ";
+    cin>>n;
+    
+    int arr[n];
+    cout<<"enter array : ";
+    for(int i =0;  i<n; i++){
+        cin>>arr[i];
+    }
+    cout << funct(arr,n);
+    return 0;
+}
