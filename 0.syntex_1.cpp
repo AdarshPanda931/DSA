c41091f41c6d9923e3872d94210049c5b2f96af7
“AdarshPanda931” <adarshpanda931@gmail.com>
2023-12-15 21:02:32 +0530

    adding 1 modifiatin and 2 more c++ files

--- 0.syntex_1.cpp
+++ 0.syntex_1.cpp
@@ -2,7 +2,19 @@
 using namespace std;
  
 int main() {
- 
-    cout << sqrt(25); 
+    int n;
+    cout<<"enter size: ";
+    cin>>n;
+    
+    int arr[n];
+    cout<<"enter array : ";
+    for(int i =0;  i<n; i++){
+        cin>>arr[i];
+    }
+    
+    cout<<"your array is : ";
+    for(int i =0;  i<n; i++){
+        cout<<arr[i]<<" ";
+    }
     return 0;
 }
